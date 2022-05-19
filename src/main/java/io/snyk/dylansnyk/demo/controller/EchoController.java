3f38fa54645b19b587159dfef9dab639939eeabb
dylansnyk <dylan.havelock@snyk.io>
2022-05-19 09:37:14 -0400

    add vuln

--- src/main/java/io/snyk/dylansnyk/demo/controller/EchoController.java
+++ src/main/java/io/snyk/dylansnyk/demo/controller/EchoController.java
@@ -1,21 +1,32 @@
 package io.snyk.dylansnyk.demo.controller;
 
 import io.snyk.dylansnyk.demo.model.EchoDto;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class EchoController {
 
+    @SneakyThrows
     @GetMapping("/echo")
     public EchoDto getEcho(@RequestParam String echo) {
+        String[] command = {echo};
+        executeCommand(command);
         return EchoDto.builder()
                 .echo(echo)
                 .build();
+    }
+
+    private void executeCommand(@NonNull final String[] command) throws IOException {
+        Process process = new ProcessBuilder(command).start();
     }
 }
