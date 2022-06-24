103b0a4b1c11c683431b59f2ef95bd64c7937ad7
Dylan Havelock <94395157+dylansnyk@users.noreply.github.com>
2022-06-24 11:17:54 -0400

    add vulnerabilities (#9)

--- src/main/java/io/snyk/dylansnyk/demo/controller/EchoController.java
+++ src/main/java/io/snyk/dylansnyk/demo/controller/EchoController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -28,5 +32,18 @@ public class EchoController {
 
     private void executeCommand(@NonNull final String[] command) throws IOException {
         Process process = new ProcessBuilder(command).start();
+    }
+
+    @SneakyThrows
+    @GetMapping("/ssrf")
+    public void makeRequest(@RequestParam String url) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
     }
 }
